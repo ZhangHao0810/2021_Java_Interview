19dd499b1153ee7dffcb63d8cb94a83a03b7f216
ZhangHao0810 <874993952@qq.com>
2021-01-14 17:07:23 +0800

    泛型

---
+++ 尚硅谷-Java高级篇-泛型/src/模拟数据库交互/CustomerDAO.java
@@ -0,0 +1,8 @@
+package 模拟数据库交互;
+
+/**
+ * @author shkstart
+ * @create 2019 上午 11:55
+ */
+public class CustomerDAO extends DAO<Customer>{//只能操作某一个表的DAO
+}
---
+++ 尚硅谷-Java高级篇-泛型/src/模拟数据库交互/Student.java
@@ -0,0 +1,8 @@
+package 模拟数据库交互;
+
+/**
+ * @author shkstart
+ * @create 2019 上午 11:58
+ */
+public class Student {
+}
